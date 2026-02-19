83aa34f2c6322fc9138dab59e1bb857a412f40fb
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-02-19 00:58:29 -0300

    os.Args

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+
+	default:
+		panic("bad command")
+	}
+
 	fmt.Println("Tente com 'axolotl' para rodar a CLI")
+}
+
+func run() {
+	fmt.Println("Running the function :)")
 }
