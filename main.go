d8544b7991617ad8bc3ac4432eb44891fbecaa8a
Joao Victor de Sousa Lima <joaovictordsl00@gmail.com>
2026-02-24 01:12:12 -0300

    run() childProcess

--- main.go
+++ main.go
@@ -11,7 +11,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-
+	case "child":
+		child()
 	default:
 		panic("bad command")
 	}
@@ -20,13 +21,31 @@ func main() {
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS,
 	}
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Erro ao rodar: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func child() {
+	fmt.Printf("Running %v\n", os.Args[2:])
+
+	syscall.Sethostname([]byte("container"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Env = append(os.Environ(), "PS1=\\h:\\w # ")
 
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Erro ao rodar: %v\n", err)
